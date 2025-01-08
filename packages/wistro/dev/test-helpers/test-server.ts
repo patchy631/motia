ObiyaDev/obiya

refactor: restore e2e

packages/wistro/dev/test-helpers/test-server.ts
@@ -1,0 +1,32 @@
+import path from 'path'
+import fs from 'fs'
+import { parse } from 'yaml'
+import { Config } from './../config.types'
+import { buildWorkflows } from './../flow-builder'
+import { createEventManager } from './../event-manager'
+import { createServer } from './../server'
+import { WistroServer, EventManager, Event } from './../../wistro.types'
+import { createWorkflowHandlers } from './../workflow-handlers'
+
+type Response = Promise<{
+  eventManager: EventManager
+  server: WistroServer
+}>
+
+export const createTestServer = async <EData>(
+  configPath: string,
+  eventSubscriber?: (event: Event<EData>) => void,
+  configOverrides?: Config,
+): Response => {
+  const configYaml = fs.readFileSync(path.join(configPath, 'config.yml'), 'utf8')
+  const config: Config = parse(configYaml)
+  const workflowSteps = await buildWorkflows()
+  const eventManager = createEventManager(eventSubscriber as (event: Event<unknown>) => void)
+  const { server } = await createServer({ ...config, ...(configOverrides ?? {}) }, workflowSteps, eventManager, {
+    skipSocketServer: true,
+  })
+
+  createWorkflowHandlers(workflowSteps, eventManager, config.state)
+
+  return { server, eventManager }
+}

packages/wistro/dev/wistro-dev.ts
@@ -3,73 +3,14 @@ import fs from 'fs'
 import { parse } from 'yaml'
 import { createServer } from './server'
 import { createWorkflowHandlers } from './workflow-handlers'
-import { getPythonConfig } from './python/get-python-config'
 import { createEventManager } from './event-manager'
-import { Config, WorkflowStep } from './config.types'
-import { FlowConfig } from '../wistro.types'
+import { Config } from './config.types'
+import { buildWorkflows } from './flow-builder'
 
 require('ts-node').register({
   transpileOnly: true,
   compilerOptions: { module: 'commonjs' },
 })
-
-async function parseWorkflowFolder(folderPath: string, nextWorkflows: WorkflowStep[]): Promise<WorkflowStep[]> {
-  const workflowFolderItems = fs.readdirSync(folderPath, { withFileTypes: true })
-  const workflowFiles = workflowFolderItems
-    .filter(({ name }) => name.endsWith('.step.ts') || name.endsWith('.step.js') || name.endsWith('.step.py'))
-    .map(({ name }) => name)
-  const workflowRootFolders = workflowFolderItems.filter((item) => item.isDirectory())
-  let workflows: WorkflowStep[] = [...nextWorkflows]
-
-  console.log('[Workflows] Building workflows', workflowFiles)
-
-  for (const file of workflowFiles) {
-    const isPython = file.endsWith('.py')
-
-    if (isPython) {
-      console.log('[Workflows] Building Python workflow', file)
-      const config = await getPythonConfig(path.join(folderPath, file))
-      console.log('[Workflows] Python workflow config', config)
-      workflows.push({ config, file, filePath: path.join(folderPath, file) })
-    } else {
-      console.log('[Workflows] Building Node workflow', file)
-      const module = require(path.join(folderPath, file))
-      if (!module.config) {
-        console.log(`[Workflows] skipping file ${file} as it does not have a valid config`)
-        continue
-      }
-      console.log(
-        `[Workflows] processing component ${module.config.name} for workflow ${module.config.tags?.workflow ?? file}`,
-      )
-      const config = module.config as FlowConfig<any>
-      workflows.push({ config, file, filePath: path.join(folderPath, file) })
-    }
-  }
-
-  if (workflowRootFolders.length > 0) {
-    for (const folder of workflowRootFolders) {
-      console.log('[Workflows] Building nested workflows in path', path.join(folderPath, folder.name))
-      const nestedWorkflows = await parseWorkflowFolder(path.join(folderPath, folder.name), [])
-      workflows = [...workflows, ...nestedWorkflows]
-    }
-  }
-
-  return workflows
-}
-
-async function buildWorkflows(): Promise<WorkflowStep[]> {
-  // Read all workflow folders under /flows directory
-  const flowsDir = path.join(process.cwd(), 'steps')
-
-  // Check if steps directory exists
-  if (!fs.existsSync(flowsDir)) {
-    console.log('No /steps directory found')
-    return []
-  }
-
-  // Get all workflow folders
-  return parseWorkflowFolder(flowsDir, [])
-}
 
 export const dev = async (): Promise<void> => {
   const configYaml = fs.readFileSync(path.join(process.cwd(), 'config.yml'), 'utf8')

playground/tests/e2e/endpointServerHandshake-redis.spec.ts
@@ -1,0 +1,74 @@
+import { test, expect } from '@playwright/test'
+import path from 'path'
+import { createTestServer, WistroServer, Event } from 'wistro'
+
+test.describe('endpointServerHandshake + Redis E2E', () => {
+  let collectedEvents: Array<Event<unknown>> = []
+  let server: WistroServer
+  let eventSubscriber = (event: Event<unknown>) => {
+    collectedEvents.push(event)
+  }
+
+  test.beforeAll(async () => {
+    const result = await createTestServer(path.join(__dirname, '../../'), eventSubscriber)
+    server = result.server
+  })
+
+  test.afterAll(async () => {
+    await server.close()
+  })
+
+  test.beforeEach(async () => {
+    // Reset our array for each test
+    collectedEvents = []
+  })
+
+  test('Verifies handshake flow & Redis events are published', async ({ page }) => {
+    // 1) Go to the Playground UI
+    await page.goto('http://localhost:3000')
+    // Wait for the workflow dropdown or a known element
+    await expect(page.locator('text=Endpoint Server Handshake')).toBeVisible()
+
+    // 2) Select 'endpointServerHandshake' in the dropdown (from config)
+    const workflowSelect = page.getByTestId('handshake')
+    await workflowSelect.click()
+
+    // Wait for some node label to appear, e.g. "Node Starter"
+    await expect(page.getByTestId('subscribes__handshake.initiate')).toBeVisible()
+
+    // 3) Trigger the workflow by calling the inbound route
+    const response = await fetch('http://localhost:3000/api/endpoint-server-handshake', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      // Optionally pass some JSON data to your workflow
+      body: JSON.stringify({
+        message: 'Hello from endpointServerHandshake test',
+      }),
+    })
+
+    expect(response.status).toBe(200)
+
+    // Wait a bit for the events to propagate
+    await page.waitForTimeout(1000)
+
+    // 4) Check Redis events
+    // Extract the eventType from each collected event
+    const eventTypes = collectedEvents.map((ev) => ev.type)
+    // We expect handshake.initiate => handshake.callApi => handshake.apiResponse
+    // Possibly the flow might skip some if there's a direct route, adjust as needed
+    // We'll just check that at least these show up somewhere.
+    expect(eventTypes).toEqual(
+      expect.arrayContaining(['handshake.initiate', 'handshake.callApi', 'handshake.apiResponse']),
+    )
+
+    // (Optional) Check the final event's data
+    const finalEvent = collectedEvents.find((e) => e.type === 'handshake.apiResponse')
+    expect(finalEvent).toBeDefined()
+    // If you want to confirm shape:
+    // expect(finalEvent.data).toHaveProperty("externalTodo");
+    // expect(finalEvent.data).toHaveProperty("userMessage");
+
+    // 5) (Optional) Confirm final node label or UI text
+    await expect(page.getByTestId('subscribes__handshake.apiResponse').last()).toBeVisible()
+  })
+})

playground/tests/e2e/parallelMergeState-redis.spec.ts
@@ -1,0 +1,69 @@
+import { test, expect } from '@playwright/test'
+import path from 'path'
+import { createTestServer, Event, WistroServer } from 'wistro'
+
+test.describe('Parallel Merge State Workflow + Redis E2E', () => {
+  let collectedEvents: Array<Event<unknown>> = []
+  let server: WistroServer
+  let eventSubscriber = (event: Event<unknown>) => {
+    collectedEvents.push(event)
+  }
+
+  test.beforeAll(async () => {
+    const result = await createTestServer(path.join(__dirname, '../../'), eventSubscriber)
+    server = result.server
+  })
+
+  test.afterAll(async () => {
+    await server.close()
+  })
+
+  test.beforeEach(async () => {
+    // Reset our array for each test
+    collectedEvents = []
+  })
+
+  test('verifies parallel merge workflow state & events', async ({ page }) => {
+    // 1. Trigger the workflow
+    const response = await fetch('http://localhost:3000/api/parallel-merge', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        message: 'Start parallel merge test',
+      }),
+    })
+    expect(response.status).toBe(200)
+
+    // Wait for events to propagate
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+
+    // 2. Verify event sequence
+    const eventTypes = collectedEvents.map((ev) => ev.type)
+
+    // Expected event sequence
+    expect(eventTypes).toEqual(
+      expect.arrayContaining([
+        'pms.initialize',
+        'pms.start',
+        'pms.stepA.done',
+        'pms.stepB.done',
+        'pms.stepC.done',
+        'pms.join.complete',
+      ]),
+    )
+
+    // 3. Get the workflow trace ID
+    const startEvent = collectedEvents.find((ev) => ev.type === 'pms.start')
+    expect(startEvent?.traceId).toBeDefined()
+
+    // 5. Verify final completion event data
+    const completionEvent = collectedEvents.find((ev) => ev.type === 'pms.join.complete')
+    expect(completionEvent).toBeDefined()
+    // expect(completionEvent.data).toMatchObject({
+    //   stepA: expect.any(Object),
+    //   stepB: expect.any(Object),
+    //   stepC: expect.any(Object),
+    //   mergedAt: expect.any(String),
+    // })
+  })
+})

patchy631/motia

packages/wistro/dev/test-helpers/test-server.ts
@@ -1,0 +1,32 @@
+import path from 'path'
+import fs from 'fs'
+import { parse } from 'yaml'
+import { Config } from './../config.types'
+import { buildFlows } from './../flow-builder'
+import { createEventManager } from './../event-manager'
+import { createServer } from './../server'
+import { WistroServer, EventManager, Event } from './../../wistro.types'
+import { createFlowHandlers } from './../flow-handlers'
+
+type Response = Promise<{
+  eventManager: EventManager
+  server: WistroServer
+}>
+
+export const createTestServer = async <EData>(
+  configPath: string,
+  eventSubscriber?: (event: Event<EData>) => void,
+  configOverrides?: Config,
+): Response => {
+  const configYaml = fs.readFileSync(path.join(configPath, 'config.yml'), 'utf8')
+  const config: Config = parse(configYaml)
+  const flowSteps = await buildFlows()
+  const eventManager = createEventManager(eventSubscriber as (event: Event<unknown>) => void)
+  const { server } = await createServer({ ...config, ...(configOverrides ?? {}) }, flowSteps, eventManager, {
+    skipSocketServer: true,
+  })
+
+  createFlowHandlers(flowSteps, eventManager, config.state)
+
+  return { server, eventManager }
+}

packages/wistro/dev/wistro-dev.ts
@@ -3,71 +3,14 @@ import fs from 'fs'
 import { parse } from 'yaml'
 import { createServer } from './server'
 import { createFlowHandlers } from './flow-handlers'
-import { getPythonConfig } from './python/get-python-config'
 import { createEventManager } from './event-manager'
-import { Config, FlowStep } from './config.types'
-import { FlowConfig } from '../wistro.types'
+import { Config } from './config.types'
+import { buildFlows } from './flow-builder'
 
 require('ts-node').register({
   transpileOnly: true,
   compilerOptions: { module: 'commonjs' },
 })
-
-async function parseFlowFolder(folderPath: string, nextFlows: FlowStep[]): Promise<FlowStep[]> {
-  const flowFolderItems = fs.readdirSync(folderPath, { withFileTypes: true })
-  const flowFiles = flowFolderItems
-    .filter(({ name }) => name.endsWith('.step.ts') || name.endsWith('.step.js') || name.endsWith('.step.py'))
-    .map(({ name }) => name)
-  const flowRootFolders = flowFolderItems.filter((item) => item.isDirectory())
-  let flows: FlowStep[] = [...nextFlows]
-
-  console.log('[Flows] Building flows', flowFiles)
-
-  for (const file of flowFiles) {
-    const isPython = file.endsWith('.py')
-
-    if (isPython) {
-      console.log('[Flows] Building Python flow', file)
-      const config = await getPythonConfig(path.join(folderPath, file))
-      console.log('[Flows] Python flow config', config)
-      flows.push({ config, file, filePath: path.join(folderPath, file) })
-    } else {
-      console.log('[Flows] Building Node flow', file)
-      const module = require(path.join(folderPath, file))
-      if (!module.config) {
-        console.log(`[Flows] skipping file ${file} as it does not have a valid config`)
-        continue
-      }
-      console.log(`[Flows] processing component ${module.config.name} for flow ${module.config.tags?.flow ?? file}`)
-      const config = module.config as FlowConfig<any>
-      flows.push({ config, file, filePath: path.join(folderPath, file) })
-    }
-  }
-
-  if (flowRootFolders.length > 0) {
-    for (const folder of flowRootFolders) {
-      console.log('[Flows] Building nested flows in path', path.join(folderPath, folder.name))
-      const nestedFlows = await parseFlowFolder(path.join(folderPath, folder.name), [])
-      flows = [...flows, ...nestedFlows]
-    }
-  }
-
-  return flows
-}
-
-async function buildFlows(): Promise<FlowStep[]> {
-  // Read all flow folders under /flows directory
-  const flowsDir = path.join(process.cwd(), 'steps')
-
-  // Check if steps directory exists
-  if (!fs.existsSync(flowsDir)) {
-    console.log('No /steps directory found')
-    return []
-  }
-
-  // Get all flow folders
-  return parseFlowFolder(flowsDir, [])
-}
 
 export const dev = async (): Promise<void> => {
   const configYaml = fs.readFileSync(path.join(process.cwd(), 'config.yml'), 'utf8')

playground/tests/e2e/endpointServerHandshake-redis.spec.ts
@@ -1,0 +1,74 @@
+import { test, expect } from '@playwright/test'
+import path from 'path'
+import { createTestServer, WistroServer, Event } from 'wistro'
+
+test.describe('endpointServerHandshake + Redis E2E', () => {
+  let collectedEvents: Array<Event<unknown>> = []
+  let server: WistroServer
+  let eventSubscriber = (event: Event<unknown>) => {
+    collectedEvents.push(event)
+  }
+
+  test.beforeAll(async () => {
+    const result = await createTestServer(path.join(__dirname, '../../'), eventSubscriber)
+    server = result.server
+  })
+
+  test.afterAll(async () => {
+    await server.close()
+  })
+
+  test.beforeEach(async () => {
+    // Reset our array for each test
+    collectedEvents = []
+  })
+
+  test('Verifies handshake flow & Redis events are published', async ({ page }) => {
+    // 1) Go to the Playground UI
+    await page.goto('http://localhost:3000')
+    // Wait for the flow dropdown or a known element
+    await expect(page.locator('text=Endpoint Server Handshake')).toBeVisible()
+
+    // 2) Select 'endpointServerHandshake' in the dropdown (from config)
+    const flowSelect = page.getByTestId('handshake')
+    await flowSelect.click()
+
+    // Wait for some node label to appear, e.g. "Node Starter"
+    await expect(page.getByTestId('subscribes__handshake.initiate')).toBeVisible()
+
+    // 3) Trigger the flow by calling the inbound route
+    const response = await fetch('http://localhost:3000/api/endpoint-server-handshake', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      // Optionally pass some JSON data to your flow
+      body: JSON.stringify({
+        message: 'Hello from endpointServerHandshake test',
+      }),
+    })
+
+    expect(response.status).toBe(200)
+
+    // Wait a bit for the events to propagate
+    await page.waitForTimeout(1000)
+
+    // 4) Check Redis events
+    // Extract the eventType from each collected event
+    const eventTypes = collectedEvents.map((ev) => ev.type)
+    // We expect handshake.initiate => handshake.callApi => handshake.apiResponse
+    // Possibly the flow might skip some if there's a direct route, adjust as needed
+    // We'll just check that at least these show up somewhere.
+    expect(eventTypes).toEqual(
+      expect.arrayContaining(['handshake.initiate', 'handshake.callApi', 'handshake.apiResponse']),
+    )
+
+    // (Optional) Check the final event's data
+    const finalEvent = collectedEvents.find((e) => e.type === 'handshake.apiResponse')
+    expect(finalEvent).toBeDefined()
+    // If you want to confirm shape:
+    // expect(finalEvent.data).toHaveProperty("externalTodo");
+    // expect(finalEvent.data).toHaveProperty("userMessage");
+
+    // 5) (Optional) Confirm final node label or UI text
+    await expect(page.getByTestId('subscribes__handshake.apiResponse').last()).toBeVisible()
+  })
+})

playground/tests/e2e/parallelMergeState-redis.spec.ts
@@ -1,0 +1,69 @@
+import { test, expect } from '@playwright/test'
+import path from 'path'
+import { createTestServer, Event, WistroServer } from 'wistro'
+
+test.describe('Parallel Merge State Flow + Redis E2E', () => {
+  let collectedEvents: Array<Event<unknown>> = []
+  let server: WistroServer
+  let eventSubscriber = (event: Event<unknown>) => {
+    collectedEvents.push(event)
+  }
+
+  test.beforeAll(async () => {
+    const result = await createTestServer(path.join(__dirname, '../../'), eventSubscriber)
+    server = result.server
+  })
+
+  test.afterAll(async () => {
+    await server.close()
+  })
+
+  test.beforeEach(async () => {
+    // Reset our array for each test
+    collectedEvents = []
+  })
+
+  test('verifies parallel merge flow state & events', async ({ page }) => {
+    // 1. Trigger the flow
+    const response = await fetch('http://localhost:3000/api/parallel-merge', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        message: 'Start parallel merge test',
+      }),
+    })
+    expect(response.status).toBe(200)
+
+    // Wait for events to propagate
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+
+    // 2. Verify event sequence
+    const eventTypes = collectedEvents.map((ev) => ev.type)
+
+    // Expected event sequence
+    expect(eventTypes).toEqual(
+      expect.arrayContaining([
+        'pms.initialize',
+        'pms.start',
+        'pms.stepA.done',
+        'pms.stepB.done',
+        'pms.stepC.done',
+        'pms.join.complete',
+      ]),
+    )
+
+    // 3. Get the flow trace ID
+    const startEvent = collectedEvents.find((ev) => ev.type === 'pms.start')
+    expect(startEvent?.traceId).toBeDefined()
+
+    // 5. Verify final completion event data
+    const completionEvent = collectedEvents.find((ev) => ev.type === 'pms.join.complete')
+    expect(completionEvent).toBeDefined()
+    // expect(completionEvent.data).toMatchObject({
+    //   stepA: expect.any(Object),
+    //   stepB: expect.any(Object),
+    //   stepC: expect.any(Object),
+    //   mergedAt: expect.any(String),
+    // })
+  })
+})