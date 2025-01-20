6a2943c653a7a3470ea4796ac8935b7e053c1df1
Sergio Marcelino <sergio@filho.org>
2025-01-20 14:50:26 -0300

    chore: try 1 trying to compile workbench

--- packages/workbench/vite.config.ts
+++ packages/workbench/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+/** @type {import('vite').UserConfig} */
+export default {
   plugins: [TanStackRouterVite(), react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+}
