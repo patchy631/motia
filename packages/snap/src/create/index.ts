9f3adcaa63ff9198ee783450f3d587716a0fd4fe
Filipe Leandro <filipeleandro87@gmail.com>
2025-04-06 20:26:55 -0300

    Fixed linting

--- packages/snap/src/create/index.ts
+++ packages/snap/src/create/index.ts
@@ -196,7 +196,7 @@ export const create = async ({ projectName, template, cursorEnabled }: Args): Pr
   // Update the path to look in the package root
   const cursorTemplateDir = path.join(__dirname, '../../../dot-files/.cursor')
   const cursorTargetDir = path.join(rootDir, '.cursor')
-  
+
   if (cursorEnabled && !checkIfDirectoryExists(cursorTargetDir)) {
     fs.cpSync(cursorTemplateDir, cursorTargetDir, { recursive: true })
     console.log('✅ .cursor folder copied')
